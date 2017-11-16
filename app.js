adf267b6c50e474cd6c3fc0a72b7ef1ac1935c71
Clement Bourgoin <cb@nokto.net>
2017-11-16 13:03:01 +0100

    Add -f option to set output filename

--- app.js
+++ app.js
@@ -5,14 +5,14 @@
 const fs    = require('fs');
 const fetch = require('node-fetch');
 
-const getImage = async function(imageWidth, imageHeight, number) {
+const getImage = async function(imageWidth, imageHeight, iteration, filename) {
 
   const url = `https://picsum.photos/${imageWidth}/${imageHeight}?random`
 
   const res = await fetch(url);
-  const dest = fs.createWriteStream(`./image${number+1}.jpg`);
+  const dest = fs.createWriteStream(`./${filename}-${iteration+1}.jpg`);
 	res.body.pipe(dest);
-  process.stdout.write(`Downloading image ${number+1} (${imageWidth}*${imageHeight})...\n`)
+  process.stdout.write(`Downloading image ${iteration+1} (${imageWidth}*${imageHeight})...\n`)
 }
 
 const program = require('commander');
@@ -22,12 +22,14 @@ program
   .option('-w, --width [width]',   'image width in pixels (default 100)')
   .option('-h, --height [height]', 'image height in pixels (default 100)')
   .option('-n, --number [number]', 'number of images to get (default 1)')
+  .option('-f, --filename [name]', 'set output filename (default image)')
   .parse(process.argv);
 
-const imageWidth  = program.width || 100;
-const imageHeight = program.height || program.width || 100;
-const number      = program.number || 1;
+const imageWidth  = program.width    || 100;
+const imageHeight = program.height   || program.width || 100;
+const number      = program.number   || 1;
+const filename    = program.filename || 'image';
 
 for (let i = 0; i < number; i++) {
-  getImage(imageWidth, imageHeight, i).catch((error) => process.stdout.write(`Error: ${error}\n`));
+  getImage(imageWidth, imageHeight, i, filename).catch((error) => process.stdout.write(`Error: ${error}\n`));
 }
