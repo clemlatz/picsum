869ac18263088e4978b1e83d7abfaeef50c2070e
Clement Bourgoin <cb@nokto.net>
2017-11-16 12:50:51 +0100

    Add -n option to set number of images to download

--- app.js
+++ app.js
@@ -5,24 +5,29 @@
 const fs    = require('fs');
 const fetch = require('node-fetch');
 
-const getImage = async function(imageWidth, imageHeight) {
+const getImage = async function(imageWidth, imageHeight, number) {
 
   const url = `https://picsum.photos/${imageWidth}/${imageHeight}?random`
 
   const res = await fetch(url);
-  const dest = fs.createWriteStream('./image.jpg');
+  const dest = fs.createWriteStream(`./image${number+1}.jpg`);
 	res.body.pipe(dest);
+  process.stdout.write(`Downloading image ${number+1} (${imageWidth}*${imageHeight})...\n`)
 }
 
 const program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-w, --width [width]', 'image width in pixels')
-  .option('-h, --height [height]','image height in pixels')
+  .option('-w, --width [width]',   'image width in pixels (default 100)')
+  .option('-h, --height [height]', 'image height in pixels (default 100)')
+  .option('-n, --number [number]', 'number of images to get (default 1)')
   .parse(process.argv);
 
 const imageWidth  = program.width || 100;
 const imageHeight = program.height || program.width || 100;
+const number      = program.number || 1;
 
-getImage(imageWidth, imageHeight);
+for (let i = 0; i < number; i++) {
+  getImage(imageWidth, imageHeight, i).catch((error) => process.stdout.write(`Error: ${error}\n`));
+}
