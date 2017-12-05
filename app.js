7a69f669749ea10490d53ab48ffd4d420161c4d4
Clement Bourgoin <cb@nokto.net>
2017-12-05 14:39:29 +0100

    Add a query parameter to search images by keywords

--- app.js
+++ app.js
@@ -5,10 +5,14 @@
 const fs    = require('fs');
 const fetch = require('node-fetch');
 
-const getImage = async function(imageWidth, imageHeight, iteration, filename, retina) {
+const getImage = async function(imageWidth, imageHeight, iteration, filename, retina, query) {
 
-  const url      = `https://source.unsplash.com/${imageWidth}x${imageHeight}`;
+  let url        = `https://source.unsplash.com/${imageWidth}x${imageHeight}`;
   const imageNum = iteration + 1;
+
+  if (query !== null) {
+    url += `/?${query}`;
+  }
 
   const res = await fetch(url);
   const dest = fs.createWriteStream(`./${filename}-${imageNum}.jpg`);
@@ -30,11 +34,12 @@ const program = require('commander');
 
 program
   .version('1.0.0')
-  .option('-w, --width [width]',   'image width in pixels (default 100)')
-  .option('-h, --height [height]', 'image height in pixels (default 100)')
-  .option('-n, --number [number]', 'number of images to get (default 1)')
-  .option('-f, --filename [name]', 'set output filename (default image)')
-  .option('-r, --retina',          'download retina versions as well (default false)')
+  .option('-w, --width [width]',    'image width in pixels (default 100)')
+  .option('-h, --height [height]',  'image height in pixels (default 100)')
+  .option('-n, --number [number]',  'number of images to get (default 1)')
+  .option('-f, --filename [name]',  'set output filename (default image)')
+  .option('-r, --retina',           'download retina versions as well (default false)')
+  .option('-q, --query [keywords]', 'keywords for searching thematic pictures')
   .parse(process.argv);
 
 const imageWidth  = program.width    || 100;
@@ -42,7 +47,8 @@ const imageHeight = program.height   || program.width || 100;
 const number      = program.number   || 1;
 const filename    = program.filename || 'image';
 const retina      = program.retina   || false;
+const query       = program.query    || null;
 
 for (let i = 0; i < number; i++) {
-  getImage(imageWidth, imageHeight, i, filename, retina).catch((error) => process.stdout.write(`Error: ${error}\n`));
+  getImage(imageWidth, imageHeight, i, filename, retina, query).catch((error) => process.stdout.write(`Error: ${error}\n`));
 }
